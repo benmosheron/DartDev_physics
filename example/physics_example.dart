605437e3e0ed2361f88c96b764a22cea54d83a5f
BenLaptop <revolverator@hotmail.com>
2016-01-28 18:02:38 +0000

    Initial commit of auto generated code

---
+++ example/physics_example.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, Ben Sheron. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library physics.example;
+
+import 'package:physics/physics.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/physics.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, Ben Sheron. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The physics library.
+///
+/// This is an awesome library. More dartdocs go here.
+library physics;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/physics_base.dart';
